25e2b497fa5162154835f85d70cd827a504cca0b
JCharante <git@jcharante.com>
2021-02-05 05:09:39 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,22 @@
-FROM nginx
+FROM python:3.8
 
-COPY ./ /usr/share/nginx/html
+ARG APP_USER=appuser
 
-CMD ["nginx", "-g", "daemon off;"]
+RUN groupadd -r ${APP_USER} && useradd --no-log-init -r -g ${APP_USER} ${APP_USER}
 
-EXPOSE 80
+RUN mkdir webapp
+
+ADD requirements.txt requirements.txt
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./ webapp/
+
+WORKDIR /webapp
+
+USER ${APP_USER}:${APP_USER}
+
+EXPOSE 3401
+
+CMD ["gunicorn", "-b 0.0.0.0:3401", "-w 3", "app:app"]
+
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import datetime
 from flask import Flask, render_template, redirect, url_for
+from typing import List, Optional, Dict, Callable
 
 app = Flask(__name__)
 
@@ -7,11 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def landing_page():
     return render_template('index.html')
-
-
-@app.route('/blog')
-def blog_home():
-    return render_template('blog/blog.html')
 
 
 class BlogInfo:
@@ -22,37 +18,107 @@ class BlogInfo:
         self.file = file
 
 
+class BlogChain:
+    def __init__(self, blog: BlogInfo):
+        self.val = blog
+        self.previous = None  # type: Optional[BlogChain]
+        self.next = None  # type: Optional[BlogChain]
+
+
 class BlogIndex:
     def __init__(self):
         self.files = {}
         self.titles = {}
         self.dates = {}
 
-    def register_blog(self, route, title, date, file):
-        self.files[route] = file
-        self.titles[route] = title
-        self.dates[route] = date
+        self.entries = {}  # type: Dict[str, BlogInfo]
+        # cached -- needs to be built
+        self.chain = None # type: Optional[BlogChain]
+
+    def register_blog(self, blog: BlogInfo):
+        self.entries[blog.route] = blog
 
     def blog_exists(self, route):
-        return route in self.files
+        return route in self.entries
 
     def get_blog_info(self, route) -> BlogInfo:
-        return BlogInfo(route, self.titles[route], self.dates[route], self.files[route])
+        return self.entries[route]
+
+    def build_chain(self):
+        entries_sorted_time = sorted(list(self.entries.values()), key=lambda BI: BI.date, reverse=False)
+        last_entry = None  # type: Optional[BlogChain]
+        for i in range(len(entries_sorted_time)):
+            if i == 0:
+                self.chain = BlogChain(entries_sorted_time[0])
+                last_entry = self.chain
+            else:
+                last_entry.next = BlogChain(entries_sorted_time[i])
+                last_entry.next.previous = last_entry
+                last_entry = last_entry.next
+
+    def get_chain(self) -> Optional[BlogChain]:
+        return self.chain
+
+    def get_entry_in_chain(self, route) -> Optional[BlogChain]:
+        cur = self.chain
+        if cur is not None:
+            while cur.val.route != route:
+                cur = cur.next
+        return cur
+
+    def get_all_blogs(self) -> List[BlogInfo]:
+        arr = []
+        for route, blog_info in self.entries.items():
+            arr.append(blog_info)
+        return arr
 
 
 blog = BlogIndex()
-blog.register_blog('test', 'Test Blog', datetime.datetime.strptime('2021-02-03 19:07', '%Y-%m-%d %H:%M'), 'test.html')
-blog.register_blog('new-platform', 'New Platform', datetime.datetime.strptime('2021-02-03 20:27', '%Y-%m-%d %H:%M'), 'random/new-platform.html')
-blog.register_blog('chau', 'As a kid I got shamed for appropriating Italian culture', datetime.datetime.strptime('2018-02-01 00:01', '%Y-%m-%d %H:%M'), 'random/chau.html')
+blog.register_blog(
+    BlogInfo('learn-word-clusters',
+             'Learn Vocabulary through Clusters',
+             datetime.datetime.strptime('2021-02-05 14:32', '%Y-%m-%d %H:%M'),
+             'random/learn-word-clusters.html'))
+"""
+blog.register_blog(
+    BlogInfo('vnese-lang-advice',
+             'Advice for learning Vietnamese',
+             datetime.datetime.strptime('2021-02-04 15:01', '%Y-%m-%d %H:%M'),
+             'random/vietnamese-lang-advice.html'))
+"""
+blog.register_blog(
+    BlogInfo('new-platform',
+             'New Platform',
+             datetime.datetime.strptime('2021-02-03 20:27', '%Y-%m-%d %H:%M'),
+             'random/new-platform.html'))
+blog.register_blog(
+    BlogInfo('test',
+             'Test Blog',
+             datetime.datetime.strptime('2021-02-03 19:07', '%Y-%m-%d %H:%M'),
+             'test.html'))
+blog.register_blog(
+    BlogInfo('chau',
+             'As a kid I got shamed for appropriating Italian culture',
+             datetime.datetime.strptime('2018-02-01 00:01', '%Y-%m-%d %H:%M'),
+             'random/chau.html'))
+blog.build_chain()
+# blog.register_blog('shipping-calculator', 'Shipping Calculator', datetime.datetime.strptime('2017-10-01 00:01', '%Y-%m-%d %H:%M'), 'random/shipping-calculator.html')
+
+
+@app.route('/blog')
+@app.route('/blog/')
+def blog_home():
+    return render_template('blog/blog.html', blog_index=blog, now=datetime.datetime.now())
 
 
 @app.route('/blog/<route>')
 def blog_handler(route: str):
     if blog.blog_exists(route):
         blog_info = blog.get_blog_info(route)
-        return render_template(f"blog/{blog_info.file}", blog_info=blog_info)
+        return render_template(f"blog/{blog_info.file}", blog_chain=blog.get_entry_in_chain(route), blog_info=blog_info, now=datetime.datetime.now())
     else:
         return redirect('/blog', 404)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
